0d8fa673134b6d2f3c68c3a84b7d60d43c590ada
bipinaghimire <bipinaghimire6@gmail.com>
2023-04-16 17:09:21 +0545

    linear search

--- Algorithms/Array/linear_search.java
+++ Algorithms/Array/linear_search.java
@@ -1,5 +1,8 @@
 package Algorithms.Array;
 
+import java.util.Arrays;
+import java.util.OptionalInt;
+
 public class linear_search {
     public static Integer linearsearch(int[] arr, int item) {
         for (int curr : arr) {
@@ -10,9 +13,14 @@ public class linear_search {
         return -9999;
     }
 
+    public static OptionalInt linearsearch2(int[] arr, int item) {
+        return Arrays.stream(arr).filter(x -> x == item).findFirst();
+    }
+
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         System.out.println(linearsearch(arr, 5));
         System.out.println(linearsearch(arr, 11));
+        System.out.println(linearsearch2(arr, 0));
     }
 }
