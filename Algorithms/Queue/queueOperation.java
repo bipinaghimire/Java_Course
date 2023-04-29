e329554966bdae6d832f5d59f4d9784e65002897
bipinaghimire <bipinaghimire6@gmail.com>
2023-04-29 10:46:10 +0545

    queue operations

---
+++ Algorithms/Queue/queueOperation.java
@@ -0,0 +1,28 @@
+package Algorithms.Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class queueOperation {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+        queue.add(6);
+
+        System.out.println(queue);
+
+        System.out.println(queue.peek());
+
+        System.out.println(queue.poll());
+
+        System.out.println(queue);
+
+        int removedItem = queue.remove();
+        System.out.println(removedItem);
+
+        System.out.println(queue);
+    }
+}
